157fb216b624097bfef2346e7371758398e78187
lgramirez <gramirez.developer@gmail.com>
2020-05-14 17:10:54 -0400

    Add sample JSON and function api/todos

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,15 @@
-const functions = require("firebase-functions");
+// const functions = require("firebase-functions");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
-});
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   response.send("Hello from Firebase!");
+// });
+
+const functions = require("firebase-functions");
+const app = require("express")();
+const { getAllTodos } = require("./APIs/todos");
+
+app.get("/todos", getAllTodos);
+exports.api = functions.https.onRequest(app);
