4181cb56ce9cad2a2baba72175e645a1c3fa5a2c
lgramirez <gramirez.developer@gmail.com>
2020-05-15 16:11:35 -0400

    Login User API

--- functions/index.js
+++ functions/index.js
@@ -15,10 +15,15 @@ const {
   deleteTodo,
   editTodo,
 } = require("./APIs/todos");
+const { loginUser } = require("./APIs/users");
 
+// Todos
 app.get("/todos", getAllTodos);
 app.post("/todo", postOneTodo);
 app.delete("/todo/:todoId", deleteTodo);
 app.put("/todo/:todoId", editTodo);
+
+// Users
+app.post("/login", loginUser);
 
 exports.api = functions.https.onRequest(app);
