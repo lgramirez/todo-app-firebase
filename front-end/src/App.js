7648377970ef5c52b03c4929d7d20e666eb8a0bb
lgramirez <gramirez.developer@gmail.com>
2020-05-22 19:40:01 -0400

    Add the home page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,6 +3,7 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
+import Home from "./pages/Home";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route exact path="/login" component={Login} />
         <Route exact path="/signup" component={SignUp} />
+        <Route exact path="/" component={Home} />
       </Switch>
     </Router>
   );
---
+++ front-end/src/pages/Home.js
@@ -0,0 +1,187 @@
+import React, { Component } from "react";
+import axios from "axios";
+import Account from "../components/Account";
+import Todo from "../component/Todo";
+import { authMiddleware } from "../util/auth";
+import {
+  Drawer,
+  AppBar,
+  Toolbar,
+  List,
+  Typography,
+  Divider,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Avatar,
+  CircularProgress,
+} from "@material-ui/core";
+import { AccountBox, Notes, ExitToApp } from "@material-ui/icons";
+// import { AccountBoxIcon } from "@material-ui/icons/AccountBox";
+import withStyles from "@material-ui/core/styles/withStyles";
+
+const drawerWidth = 240;
+const styles = (theme) => ({
+  root: {
+    display: "flex",
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+  avatar: {
+    height: 110,
+    width: 100,
+    flexShrink: 0,
+    flexGrow: 0,
+    marginTop: 20,
+  },
+  uiProgress: {
+    position: "fixed",
+    zIndex: "1000",
+    height: "31px",
+    width: "31px",
+    left: "50%",
+    top: "35%",
+  },
+  toolBar: theme.mixings.toolbal,
+});
+
+class Home extends Component {
+  state = {
+    render: false,
+  };
+
+  loadAccountPage = (event) => {
+    this.setState({ render: true });
+  };
+
+  loadTodoPage = (event) => {
+    this.setState({ render: false });
+  };
+
+  logoutHandler = (event) => {
+    localStorage.removeItem("AuthToken");
+    this.props.history.push("/login");
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      firstName: "",
+      lastName: "",
+      profilePicture: "",
+      uiLoading: false,
+      imageLoading: false,
+    };
+  }
+
+  componentWillMount = () => {
+    authMiddleware(this.props.history);
+    const authToken = localStorage.getItem("AuthToken");
+    axios.defaults.headers.common = { Authorization: authToken };
+    axios
+      .get("/user")
+      .then((response) => {
+        console.log(response.data);
+        this.setState({
+          firstName: response.data.userCredentials.firstName,
+          lastName: response.data.userCredentials.lastName,
+          email: response.data.userCredentials.email,
+          phoneNumber: response.data.userCredentials.phoneNumber,
+          country: response.data.userCredentials.country,
+          username: response.data.userCredentials.username,
+          uiLoading: false,
+          profilePicture: response.data.userCredentials.imageUrl,
+        });
+      })
+      .catch((error) => {
+        if (error.response.status === 403) {
+          this.props.history.push("/login");
+        }
+        console.log(error);
+        this.setState({ errorMsg: "Error in retrieving the data" });
+      });
+  };
+
+  render() {
+    const { classes } = this.props;
+    if (this.state.uiLoading === true) {
+      return (
+        <div className={classes.root}>
+          {this.state.uiLoading && (
+            <CircularProgress size={150} className={classes.uiProgress} />
+          )}
+        </div>
+      );
+    } else {
+      return (
+        <div className={classes.root}>
+          <AppBar position="fixed" className={classes.appBar}>
+            <Toolbar>
+              <Typography variant="h6" noWrap>
+                TodoApp
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <Drawer
+            className={classes.drawer}
+            variant="permanent"
+            classes={{ paper: classes.drawerPaper }}
+          >
+            <div className={classes.toolBar} />
+            <Divider>
+              <center>
+                <Avatar
+                  src={this.state.profilePicture}
+                  className={classes.avatar}
+                />
+                <p>
+                  {" "}
+                  {this.state.firstName} {this.state.lastName}
+                </p>
+              </center>
+            </Divider>
+            <List>
+              <ListItem button key="Todo" onClick={this.loadTodoPage}>
+                <ListItemIcon>
+                  {" "}
+                  <NotesIcon />{" "}
+                </ListItemIcon>
+                <ListItemText primary="Todo" />
+              </ListItem>
+              <ListItem button key="Account" onClick={this.loadAccountPage}>
+                <ListItemIcon>
+                  {" "}
+                  <AccountBoxIcon />{" "}
+                </ListItemIcon>
+                <ListItemText primary="Account" />
+              </ListItem>
+              <ListItem button key="Logout" onClick={this.logoutHandler}>
+                <ListItemIcon>
+                  {" "}
+                  <ExitToApp />{" "}
+                </ListItemIcon>
+                <ListItemText primary="Logout" />
+              </ListItem>
+            </List>
+          </Drawer>
+          <div>{this.state.render ? <Account /> : <Todo />}</div>
+        </div>
+      );
+    }
+  }
+}
+
+export default withStyles(styles)(Home);
