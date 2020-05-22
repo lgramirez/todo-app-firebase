6fbc80d8401e4981a50c86b24c06cb74548655d2
lgramirez <gramirez.developer@gmail.com>
2020-05-22 16:32:21 -0400

    Remove cssbaseline to a parent component

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
 import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
@@ -98,7 +97,6 @@ class Login extends Component {
 
     return (
       <Container component="main" maxWidth="xs">
-        <CssBaseline />
         <div className={classes.paper}>
           <Avatar className={classes.avatar}>
             <LockOutlinedIcon />
